ea30c1f8c43b30f69669b7b4a961eb356d870f4f
Mauro Sanchez <maurosanchez.work@gmail.com>
2022-11-12 23:51:17 -0300

    Added cooldown for shooting

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -7,6 +7,12 @@ public class Gun : MonoBehaviour
     [SerializeField] private Bullet bulletPrefab = null;
     [SerializeField] private float bulletSpeed;
 
+    [Header("Gun Configuration")] 
+    [SerializeField] private float cooldownBetweenShots = .2f;
+    [SerializeField] private bool resetCooldownOnShotRelease = false;
+
+    private float cooldownTimer = 0;
+
     private void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -17,7 +23,19 @@ public class Gun : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, 0, angle);
 
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0) && resetCooldownOnShotRelease)
+        {
+            cooldownTimer = 0;
+        }
+        
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+            return;
+        }
+        
+
+        if (Input.GetMouseButton(0))
         {
             ShotBullet(mouseDelta);
         }
@@ -30,5 +48,7 @@ public class Gun : MonoBehaviour
         Vector3 direction = mouseDelta - transform.position;
         direction.Normalize();
         instance.Shot(direction, bulletSpeed);
+
+        cooldownTimer = cooldownBetweenShots;
     }
 }
