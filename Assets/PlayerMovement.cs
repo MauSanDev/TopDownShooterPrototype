0d264884d86c0a47f805398049a05226a39ece94
Mauro Sanchez <maurosanchez.work@gmail.com>
2022-11-14 09:27:11 -0300

    Added roll mechanic draft on Player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,14 +1,64 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 10;
+    [SerializeField] private float rollSpeed = 10;
+    [SerializeField] private SpriteRenderer spriteRenderer = null;
+
+    private Coroutine rollingRoutine = null;
+
+    [SerializeField] private float rollDistance = 5f;
+    private Vector3 rollPosition = Vector3.zero;
+    private Vector2 lastInput = Vector2.zero;
+    
     
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        if(IsRolling) return;
         
-        transform.Translate(horizontal, vertical, 0);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        bool isMoving = horizontal != 0 || vertical != 0;
+        Vector2 input = new Vector2(horizontal, vertical) * movementSpeed * Time.deltaTime;
+
+        if (isMoving)
+        {
+            lastInput = input.normalized;
+        }
+        
+        transform.Translate(input);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rollingRoutine = StartCoroutine(RollRoutine());
+        }
+    }
+
+    private bool IsRolling => rollingRoutine != null;
+
+    private IEnumerator RollRoutine()
+    {
+        rollPosition = transform.position + (Vector3)lastInput * rollDistance;
+
+        spriteRenderer.color = Color.yellow;
+        while (Vector3.Distance(transform.position, rollPosition) > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, rollPosition, rollSpeed * Time.deltaTime);
+            
+            yield return null;
+        }
+
+        spriteRenderer.color = Color.white;
+        rollingRoutine = null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 pos = transform.position + (Vector3)lastInput * rollDistance;
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(pos, 1f);
     }
 }
